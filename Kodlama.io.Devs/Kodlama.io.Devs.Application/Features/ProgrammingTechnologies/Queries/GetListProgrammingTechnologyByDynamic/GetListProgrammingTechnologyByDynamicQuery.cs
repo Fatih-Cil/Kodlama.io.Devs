1279f137d500ab00582ef84096aabe86f3daa8d7
AyrizMBP <AyrizMBP@MBP>
2022-09-11 12:27:03 +0300

    Dynamic Query ok.

---
+++ Kodlama.io.Devs/Kodlama.io.Devs.Application/Features/ProgrammingTechnologies/Queries/GetListProgrammingTechnologyByDynamic/GetListProgrammingTechnologyByDynamicQuery.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using Core.Application.Requests;
+using Core.Persistence.Dynamic;
+using Core.Persistence.Paging;
+using Kodlama.io.Devs.Application.Features.ProgrammingTechnologies.Models;
+using Kodlama.io.Devs.Application.Services.Repositories;
+using Kodlama.io.Devs.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kodlama.io.Devs.Application.Features.ProgrammingTechnologies.Queries.GetListProgrammingTechnologyByDynamic
+{
+    public class GetListProgrammingTechnologyByDynamicQuery : IRequest<ProgrammingTechnologiesListModel>
+    {
+        public Dynamic Dynamic { get; set; }
+        public PageRequest PageRequest { get; set; }
+
+        public class GetListProgrammingTechnologyByDynamicQueryHandler : IRequestHandler<GetListProgrammingTechnologyByDynamicQuery, ProgrammingTechnologiesListModel>
+        {
+            private readonly IProgrammingTechnologyRepository _programmingTechnologyRepository;
+            private readonly IMapper _mapper;
+
+            public GetListProgrammingTechnologyByDynamicQueryHandler(IProgrammingTechnologyRepository programmingTechnologyRepository, IMapper mapper)
+            {
+                _programmingTechnologyRepository = programmingTechnologyRepository;
+                _mapper = mapper;
+            }
+
+            public async Task<ProgrammingTechnologiesListModel> Handle(GetListProgrammingTechnologyByDynamicQuery request, CancellationToken cancellationToken)
+            {
+                //dynamic olarak parametre girildiğinde işleyecek.
+                IPaginate<ProgrammingTechnology> programmingTechnologies = await _programmingTechnologyRepository.GetListByDynamicAsync(request.Dynamic,
+                    include:
+                      m => m.Include(c => c.ProgrammingLanguage),
+                      index: request.PageRequest.Page,
+                      size: request.PageRequest.PageSize
+                      );
+                ProgrammingTechnologiesListModel mappedprogrammingTechnologiesListModel = _mapper.Map<ProgrammingTechnologiesListModel>(programmingTechnologies);
+                return mappedprogrammingTechnologiesListModel;
+
+
+            }
+        }
+    }
+}
--- Kodlama.io.Devs/Kodlama.io.Devs.WebAPI/Controllers/ProgrammingTechnologiesController.cs
+++ Kodlama.io.Devs/Kodlama.io.Devs.WebAPI/Controllers/ProgrammingTechnologiesController.cs
@@ -1,4 +1,5 @@
 ﻿using Core.Application.Requests;
+using Core.Persistence.Dynamic;
 using Kodlama.io.Devs.Application.Features.ProgrammingLanguages.Commands.UpdateProgrammingLanguage;
 using Kodlama.io.Devs.Application.Features.ProgrammingLanguages.Dtos;
 using Kodlama.io.Devs.Application.Features.ProgrammingTechnologies.Commands.CreateProgrammingTechnology;
@@ -7,6 +8,7 @@ using Kodlama.io.Devs.Application.Features.ProgrammingTechnologies.Commands.Upda
 using Kodlama.io.Devs.Application.Features.ProgrammingTechnologies.Dtos;
 using Kodlama.io.Devs.Application.Features.ProgrammingTechnologies.Models;
 using Kodlama.io.Devs.Application.Features.ProgrammingTechnologies.Queries.GetListProgrammingTechnology;
+using Kodlama.io.Devs.Application.Features.ProgrammingTechnologies.Queries.GetListProgrammingTechnologyByDynamic;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -44,6 +46,38 @@ namespace Kodlama.io.Devs.WebAPI.Controllers
             GetListProgrammingTechnologyQuery getListProgrammingTechnologyQuery = new GetListProgrammingTechnologyQuery { PageRequest = pageRequest };
             ProgrammingTechnologiesListModel result = await Mediator.Send(getListProgrammingTechnologyQuery);
             return Ok(result);
+
+        }
+
+        [HttpPost("Getlist/ByDynamic")]
+        public async Task<IActionResult> GetListByDynamic([FromQuery] PageRequest pageRequest,[FromBody] Dynamic dynamic)
+        {
+            GetListProgrammingTechnologyByDynamicQuery getListProgrammingTechnologyByDynamicQuery = new GetListProgrammingTechnologyByDynamicQuery { PageRequest = pageRequest, Dynamic=dynamic };
+            ProgrammingTechnologiesListModel result = await Mediator.Send(getListProgrammingTechnologyByDynamicQuery);
+            return Ok(result);
+
+//Örnek sorgu aşağıdaki gibi yapılabilir.  isminde net veya j harfi olanları listeliyoruz.Filters bölümünde virgul atarak birden çok parametre geçilebilir. operator "gte" büyük eşit demek. "lte" küçük eşit dmeek. "eq" eşit demek.
+//            {
+//  "sort": [
+//    {
+//      "field": "name",
+//      "dir": "asc"
+//    }
+//  ],
+//  "filter": {
+//    "field": "name",
+//    "operator": "contains",
+//    "value": "net",
+//    "logic": "or",
+//    "filters": [
+//{
+//      "field": "name",
+//      "operator": "contains",
+//      "value": "j"
+//    }
+//]
+//  }
+//}
 
         }
 
